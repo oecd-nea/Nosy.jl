6b2dc11d3ced5ac4862822a880a538b2c68ed93a
GKrivtchik <47811556+GKrivtchik@users.noreply.github.com>
2024-12-10 10:50:16 +0100

    Added Snapshot cost.

--- src/system/snapshots/metrics/costs.jl
+++ src/system/snapshots/metrics/costs.jl
@@ -25,3 +25,24 @@ Throw an error is there is no component with name `cname` in `s`.
 """
 cost(s::Snapshot, cname::String) = _applymetric(s, cname, cost)
 cost(s::Snapshot, cname::String, type::Symbol) = _applymetric(s, cname, cost, type)
+
+"""
+    overnightcost(s::Snapshot)
+Return the overnight cost of Snapshot `s` as the sum of overnight costs of the components of `s`.
+"""
+overnightcost(s::Snapshot) = sum(overnightcost(s, cname) for cname in keys(s.components))
+overnightcost(s::Snapshot, type::Symbol) = sum(overnightcost(s, cname, type) for cname in keys(s.components))
+
+"""
+    variablecost(s::Snapshot)
+Return the variable cost of Snapshot `s` as the sum of variable costs of the components of `s`.
+"""
+variablecost(s::Snapshot) = sum(variablecost(s, cname) for cname in keys(s.components))
+variablecost(s::Snapshot, type::Symbol) = sum(variablecost(s, cname, type) for cname in keys(s.components))
+
+"""
+    cost(s::Snapshot)
+Return the cost of the Snapshot, defined as the sum of the costs of the components of the Snapshot.
+"""
+cost(s::Snapshot) = sum(cost(s, cname) for cname in keys(s.components))
+cost(s::Snapshot, type::Symbol) = sum(cost(s, cname, type) for cname in keys(s.components))
--- test/system/snapshots/metrics/costs.jl
+++ test/system/snapshots/metrics/costs.jl
@@ -13,14 +13,15 @@ using ArgCheck: ArgumentError
 
     tsim() = Sim(TimeMesh(fill(1//2, 10)), Model())
 
-    function makeconv(s, vb)
+    # snapshot with one component
+    function makeconv(s, vb, cname="comp")
         mc = MassCarrier("m", s, energy=[1,2,3,4,5])
         ec = EnergyCarrier("e", s)
         d = BasicConverter(
             mc,
             ec,
         )   
-        c = Component("comp", d, vb)
+        c = Component(cname, d, vb)
         return c
     end
 
@@ -42,6 +43,9 @@ using ArgCheck: ArgumentError
 
         @test cost(s, "comp") == AffExpr(5. * 10.) + balance(getcomponent(s, "comp"), :input, energy, collapse=true, aggregate=true) * 2.
 
+        # Snapshot cost
+        @test cost(s) == cost(s, "comp") # only component is c
+
     end
 
 
@@ -58,6 +62,10 @@ using ArgCheck: ArgumentError
         @test cost(s, "comp", :overnight) == overnightcost(c, :overnight)
         @test cost(s, "comp", :fuel) == variablecost(c, :fuel)
         @test cost(s, "comp", :vom) == variablecost(c, :vom)
+        @test cost(s, :overnight) == overnightcost(c, :overnight)
+        @test cost(s, :fuel) == variablecost(c, :fuel)
+        @test cost(s, :vom) == variablecost(c, :vom)
+
 
     end
 
@@ -68,6 +76,7 @@ using ArgCheck: ArgumentError
         @test overnightcost(s, "comp") == 0.
         @test variablecost(s, "comp") == 0.
         @test cost(s, "comp") == 0.
+        @test cost(s) == 0.
 
     end
 
@@ -78,6 +87,48 @@ using ArgCheck: ArgumentError
         @test_throws AssertionError overnightcost(s, "nocomp")
         @test_throws AssertionError variablecost(s, "nocomp")
         @test_throws AssertionError cost(s, "nocomp")
+
+    end
+
+
+    # snapshot with 2 components
+    function makesnapshot2(vb)
+        s = tsim()
+        c1 = makeconv(s, vb, "comp1")
+        c2 = makeconv(s, vb, "comp2")
+        n1 = Node("mass node 1", c1.model.data.input) # mass carrier node
+        n2 = Node("mass node 2", c2.model.data.input) # mass carrier node
+        sn = Snapshot(s)
+        connect!(sn, c1, n1)
+        connect!(sn, c2, n2)
+        return sn
+    end
+
+
+    let s = makesnapshot2([FixedCapacity("input", mass, 5.), OvernightCost(:overnight, "input", mass, 10.), VariableCost(:fuel, "input", energy, 2.), VariableCost(:vom, "output", energy, 3.)])
+
+        # check variable cost is non-zero
+        @test (variablecost(s) isa AffExpr) && !iszero(variablecost(s))
+
+        @test variablecost(s) == variablecost(s, "comp1") + variablecost(s, "comp2")
+
+        @test variablecost(s, :fuel) == variablecost(s, "comp1", :fuel) + variablecost(s, "comp2", :fuel)
+
+        # check overnight cost is non-zero
+        @test (overnightcost(s) isa AffExpr) && !iszero(overnightcost(s))
+
+        @test overnightcost(s) == overnightcost(s, "comp1") + overnightcost(s, "comp2")
+
+        @test overnightcost(s, :overnight) == overnightcost(s, "comp1", :overnight) + overnightcost(s, "comp2", :overnight)
+
+        # check cost is non-zero
+        @test (cost(s) isa AffExpr) && !iszero(cost(s))
+
+        @test cost(s) == cost(s, "comp1") + cost(s, "comp2")
+
+        @test cost(s, :overnight) == cost(s, "comp1", :overnight) + cost(s, "comp2", :overnight)
+
+        @test cost(s, :fuel) == cost(s, "comp1", :fuel) + cost(s, "comp2", :fuel)
 
     end
 
