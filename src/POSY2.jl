dc34a166c8ec648879132305beadd917416a8921
GKrivtchik <47811556+GKrivtchik@users.noreply.github.com>
2024-12-11 15:12:46 +0100

    Added exports for user-facing functions.

--- src/POSY2.jl
+++ src/POSY2.jl
@@ -1,5 +1,30 @@
 module POSY2
 
+export TimeMesh, Sim
+export MassCarrier, EnergyCarrier, CO2Carrier
+
+export BasicConverter
+export Demand
+export DispatchableSource, ProfileSource
+export LazyStorage
+
+export VariableCapacity, FixedCapacity
+export VariableCost, OvernightCost
+
+export LinkedJointFlow, FreeJointFlow, FixedJointFlow
+
+export Component, Node, Snapshot
+export connect!
+
+export capacity
+export cost, variablecost, overnightcost
+
+export balance
+export mass, energy, co2
+
+export finalize!, optimize!
+export extract
+
 include("simulation/_includes.jl")
 include("system/_includes.jl")
 include("opti/_includes.jl")
