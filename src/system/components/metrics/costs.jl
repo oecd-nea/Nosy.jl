b8f7bdc8c7e269e765044c3ab41571f815f39ef6
GKrivtchik <47811556+GKrivtchik@users.noreply.github.com>
2025-01-23 17:16:57 +0100

    Cleaned-up definition of cost metrics.

--- src/system/components/metrics/costs.jl
+++ src/system/components/metrics/costs.jl
@@ -2,38 +2,66 @@ _metrictype(c::AbstractCostBehavior) = _costtype(c)
 
 """
     fixedcost(c::Component)
-Return the fixed cost of Component `c` as the sum of its fixed costs.
+Return the fixed cost of Component `c`.
 If the component has no fixed cost, return zero.
 """
 fixedcost(c::Component{T}) where T = sumofmetric(c, FixedCostBehavior{T}, _fixedcost)
 
+"""
+    fixedcost(c::Component, type::Symbol)
+Return fixed cost cost of type `type` of Component `c`.
+If the component has no fixed cost, return zero.
+"""
 fixedcost(c::Component{T}, type::Symbol) where T = sumofmetric(c, FixedCostBehavior{T}, _fixedcost, type)
 
 """
     variablecost(c::Component)
-Return the variable cost of Component `c` as the sum of its variable costs.
+Return the variable cost of Component `c`.
 If the component has no variable cost, return zero.
 """
 variablecost(c::Component{T}) where T = sumofmetric(c, VariableCostBehavior{T}, _variablecost)
 
+"""
+    variablecost(c::Component, type::Symbol)
+Return variable cost cost of type `type` of Component `c`.
+If the component has no variable cost, return zero.
+"""
 variablecost(c::Component{T}, type::Symbol) where T = sumofmetric(c, VariableCostBehavior{T}, _variablecost, type)
 
 """
     noloadcost(c::Component)
-Return the no-load cost of Component `c` as the sum of its no-load costs.
+Return the no-load cost of Component `c`.
 If the component has no no-load cost, return zero.
 """
 noloadcost(c::Component{T}) where T = sumofmetric(c, NoLoadCostBehavior{T}, _noloadcost)
 
+"""
+    noloadcost(c::Component, type::Symbol)
+Return the no-load cost of type `type` of Component `c`.
+If the component has no no-load cost, return zero.
+"""
 noloadcost(c::Component{T}, type::Symbol) where T = sumofmetric(c, NoLoadCostBehavior{T}, _noloadcost, type)
 
 
-const COST_COMPONENT_METRICS = (fixedcost, variablecost, noloadcost,)
+# dict containing the cost metrics (other than costs)
+# these metrics must be such that their sum is equal to cost
+const COST_COMPONENT_METRICS = Dict(
+    variablecost => "variable cost",
+    fixedcost => "fixed cost",
+    noloadcost => "no-load cost",
+)
+
+
+# total cost is defined as the sum of the costs above
 
 """
     cost(c::Component)
 Return the cost of Component `c`.
 """
-cost(c::Component) = sum(f(c) for f in COST_COMPONENT_METRICS)
+cost(c::Component) = sum(f(c) for (f,_) in COST_COMPONENT_METRICS)
 
-cost(c::Component, type::Symbol) = sum(f(c, type) for f in COST_COMPONENT_METRICS)
+"""
+    cost(c::Component)
+Return the cost of type `type` of Component `c`.
+"""
+cost(c::Component, type::Symbol) = sum(f(c, type) for (f,_) in COST_COMPONENT_METRICS)
--- src/system/snapshots/metrics/costs.jl
+++ src/system/snapshots/metrics/costs.jl
@@ -1,68 +1,70 @@
 """
-    fixedcost(s::Snapshot, cname::String)
-Return the fixed cost of Component with name `cname` in Snapshot `s`.
-If the component has no fixed cost, return zero.
-Throw an error is there is no component with name `cname` in `s`.
-"""
-fixedcost(s::Snapshot, cname::String) = _applymetric(s, cname, fixedcost)
-fixedcost(s::Snapshot, cname::String, type::Symbol) = _applymetric(s, cname, fixedcost, type)
-
-
-"""
-    variablecost(s::Snapshot, cname::String)
-Return the variable cost of Component with name `cname` in Snapshot `s`.
-If the component has no variable cost, return zero.
-Throw an error is there is no component with name `cname` in `s`.
-"""
-variablecost(s::Snapshot, cname::String) = _applymetric(s, cname, variablecost)
-variablecost(s::Snapshot, cname::String, type::Symbol) = _applymetric(s, cname, variablecost, type)
-
-"""
-    noloadcost(s::Snapshot, cname::String)
-Return the no-load cost of Component with name `cname` in Snapshot `s`.
-If the component has no no-load cost, return zero.
-Throw an error is there is no component with name `cname` in `s`.
-"""
-noloadcost(s::Snapshot, cname::String) = _applymetric(s, cname, noloadcost)
-noloadcost(s::Snapshot, cname::String, type::Symbol) = _applymetric(s, cname, noloadcost, type)
-
-"""
     cost(s::Snapshot, cname::String)
 Return the cost of Component with name `cname` in Snapshot `s`.
 If the component has no cost, return zero.
 Throw an error is there is no component with name `cname` in `s`.
 """
 cost(s::Snapshot, cname::String) = _applymetric(s, cname, cost)
+
+"""
+    cost(s::Snapshot, cname::String, type::Symbol)
+Return the cost of type `type` of Component with name `cname` in Snapshot `s`.
+If the component has no cost, return zero.
+Throw an error is there is no component with name `cname` in `s`.
+"""
 cost(s::Snapshot, cname::String, type::Symbol) = _applymetric(s, cname, cost, type)
 
 """
-    fixedcost(s::Snapshot)
-Return the fixed cost of Snapshot `s` as the sum of fixed costs of the components of `s`.
-"""
-fixedcost(s::Snapshot) = sum(fixedcost(s, cname) for cname in keys(s.components))
-fixedcost(s::Snapshot, type::Symbol) = sum(fixedcost(s, cname, type) for cname in keys(s.components))
-
-"""
-    variablecost(s::Snapshot)
-Return the variable cost of Snapshot `s` as the sum of variable costs of the components of `s`.
-"""
-variablecost(s::Snapshot) = sum(variablecost(s, cname) for cname in keys(s.components))
-variablecost(s::Snapshot, type::Symbol) = sum(variablecost(s, cname, type) for cname in keys(s.components))
-
-"""
-    noloadcost(s::Snapshot)
-Return the no-load cost of Snapshot `s` as the sum of no-load costs of the components of `s`.
-"""
-noloadcost(s::Snapshot) = sum(noloadcost(s, cname) for cname in keys(s.components))
-noloadcost(s::Snapshot, type::Symbol) = sum(noloadcost(s, cname, type) for cname in keys(s.components))
-
-"""
     cost(s::Snapshot)
-Return the cost of the Snapshot, defined as the sum of the costs of the components of the Snapshot.
+Return the sum of the costs of the components of the Snapshot.
 """
 cost(s::Snapshot) = sum(cost(s, cname) for cname in keys(s.components))
+
+"""
+    cost(s::Snapshot, type::Symbol)
+Return the sum of the costs of type `type` of the components of the Snapshot.
+"""
 cost(s::Snapshot, type::Symbol) = sum(cost(s, cname, type) for cname in keys(s.components))
 
+
+# all the cost items behave the same way
+# using code generation to reduce size
+for (metric, comment) in COST_COMPONENT_METRICS
+    mname = Symbol(metric)
+    @eval begin
+
+        @doc """
+            $($mname)(s::Snapshot, cname::String)
+        Return the $($comment) of Component with name `cname` in Snapshot `s`.
+        If the component has no $($comment), return zero.
+        Throw an error is there is no component with name `cname` in `s`.
+        """
+        $(mname)(s::Snapshot, cname::String) = _applymetric(s, cname, $(mname))
+
+        @doc """
+            $($mname)(s::Snapshot, cname::String, type::Symbol)
+        Return the $($comment) of type `type` of Component with name `cname` in Snapshot `s`.
+        If the component has no $($comment) of type `type`, return zero.
+        Throw an error is there is no component with name `cname` in `s`.
+        """
+        $(mname)(s::Snapshot, cname::String, type::Symbol) = _applymetric(s, cname, $(mname), type)
+
+
+        @doc """
+            $($mname)(s::Snapshot)
+        Return the sum of $($comment) items of the components of `s`.
+        """
+        $(mname)(s::Snapshot) = sum($(mname)(s, cname) for cname in keys(s.components))
+
+        @doc """
+            $($mname)(s::Snapshot, type::Symbol)
+        Return the sum of $($comment) items of type `type` of the components of `s`.
+        """
+        $(mname)(s::Snapshot, type::Symbol) = sum($(mname)(s, cname, type) for cname in keys(s.components))
+
+    end
+
+end
 
 
 # return a Vector{Symbol} containing all the user-defined cost types of the Snapshot
