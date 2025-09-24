d3c29dec88d35deaca75df5e0e1fd9bd1d7f7120
GKrivtchik <47811556+GKrivtchik@users.noreply.github.com>
2025-09-24 12:21:19 +0200

    Cleaned up AbstractJointFlow interface docstrings.

--- src/system/components/abstract.jl
+++ src/system/components/abstract.jl
@@ -43,8 +43,31 @@ abstract type AbstractRegularBehavior{T} <: AbstractBehavior{T} end
 
 """
 Abstract types for joint flows.
+
+Interface in file: jointflows/abstract.jl
+"""
+
+"""
+AbstractJointFlowData: supertype for joint flow data.
+Joint flow data encapsulates the data required to generate behaviors.
+It is directly constructed by the user call to the constructor.
+It behaves very similarly to AbstractBehaviorData.
+
+AbstractJointFlowData interface:
+  * implement buildjointflow(c::Component, cname::String, b::AbstractBehaviorData) -> return a AbstractBehavior
 """
 
 abstract type AbstractJointFlowData <: AbstractBehaviorData end
+
+"""
+AbstractJointFlow: supertype for joint flows.
+Joint flows themselves are not constructed by the user. 
+The constructor is called by the component constructor instead.
+
+AbstractJointFlow interface:
+  * implement jointflowname(::AbstractJointFlow)::String -> return a String (category of joint flow name e.g. fixed, linked etc.)
+  * implement name(::AbstractJointFlow)::String -> return a String (name of the current flow e.g. CO2, electricity etc.)
+  * implement mustconnect(::AbstractJointFlow)::Bool -> return a Bool (true if the user must use connect! to connect the associated port, false if not)
+"""
 
 abstract type AbstractJointFlow{T} <: AbstractBehavior{T} end
--- src/system/components/behaviors/jointflows/abstract.jl
+++ src/system/components/behaviors/jointflows/abstract.jl
@@ -1,28 +1,5 @@
 """
-Abstract types for joint flows data and joint flows.
-"""
-
-"""
-AbstractJointFlowData: supertype for joint flow data.
-Joint flow data encapsulates the data required to generate behaviors.
-It is directly constructed by the user call to the constructor.
-It behaves very similarly to AbstractBehaviorData.
-"""
-
-# abstract type AbstractJointFlowData <: AbstractBehaviorData end
-
-
-"""
-AbstractJointFlow: supertype for joint flows.
-Joint flows themselves are not constructed by the user. 
-The constructor is called by the component constructor instead.
-"""
-
-# abstract type AbstractJointFlow{T} <: AbstractBehavior{T} end
-
-"""
-AbstractJointFlowData interface:
-  * implement buildjointflow(c::Component, cname::String, b::AbstractBehaviorData) -> return a AbstractBehavior
+General functions for all joint flows.
 """
 
 buildbehavior(c::Component, b::AbstractJointFlowData) = buildjointflow(c, b)
